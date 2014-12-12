5dce93cd3d9102c82da248c0c1fdb438c11bce52
salexseen <salexseen@gmail.com>
2014-12-12 20:08:21 +0300

    fix

--- public/js/main.js
+++ public/js/main.js
@@ -109,7 +109,7 @@ var collisionPacker = function(game){
             game.load.image('terrain', 'public/game/img/terrain.png');
 
             game.load.image('player', 'public/game/img/player.png');
-            game.load.spritesheet('coin', 'public/game/img/coin.png', 44, 40, 10);
+            game.load.spritesheet('coin', 'public/game/img/coin_tile.png', 32, 32, 8);
             game.load.audio('coin_sound', ['public/game/audio/coin.mp3', 'public/game/audio/coin.waw']);
 
         },
@@ -119,13 +119,15 @@ var collisionPacker = function(game){
 
             packer = new collisionPacker(game);
 
+            player = game.add.sprite(150, 150, 'player');
             coins = game.add.group();
+            coins.z = 10;
             coinAudio = game.add.audio('coin_sound');
             coinText = game.add.text(ui.coins.x, ui.coins.y, ui.coins.text(), ui.coins.style);
             coinText.fixedToCamera = true;
             ui.coins.obj = coinText;
 
-            player = game.add.sprite(150, 150, 'player');
+
             map = game.add.tilemap('level-1');
 
 
@@ -153,31 +155,43 @@ var collisionPacker = function(game){
             game.camera.follow(player);
             cursors = game.input.keyboard.createCursorKeys();
 
-            var playerMaterial = game.physics.p2.createMaterial('playerMaterial', player.body);
-            var coinMaterial = game.physics.p2.createMaterial('coinMaterial');
-
 
             if (objectCoin.length){
                 for (var x = 0, len = objectCoin.length; x < len; x+=1){
                     var data = objectCoin[x];
 
-                    coin = coins.create(data.x, data.y, 'coin');
+                    coin = coins.create(data.x + 16, data.y + 16, 'coin');
+                    game.physics.p2.enable(coin, true);
+                    coin.body.setCircle(16);
+                    coin.body.damping = 0;
+                    coin.body.static = true;
+                    coin.body.fixedRotation = true;
+                    coin.body.data.shapes[0].sensor = true;
                     coin.animations.add('coin');
                     coin.animations.play('coin', 15, true);
                 }
             }
 
 
+            player.body.onBeginContact.add(function(body){
+                if (body && body.sprite){
+                    var sprite = body.sprite;
+                    if (sprite && sprite.alive){
+                        var key = sprite.key;
+                        if (key == 'coin'){
+                            ui.coins.setCoins();
+                            coinAudio.play();
+                            sprite.kill();
+                        }
+                    }
+                }
+            });
+
+
         },
         update: function(){
 
-            packer.contactSpriteAndGroup(player, coins, function(obj1, obj2){
-                if (obj2.alive){
-                    coinAudio.play();
-                    ui.coins.setCoins();
-                    obj2.kill();
-                }
-            });
+
 
             if (cursors.left.isDown){
                 player.body.moveLeft(150);
